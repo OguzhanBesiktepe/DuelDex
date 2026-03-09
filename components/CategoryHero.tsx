0f561013e6ce548be90a78ee931359f9ed83fccd
OguzhanBesiktepe <oguzhanbesiktepe@gmail.com>
2026-03-09 16:26:51 -0400

    Added the hero features (hovering, Navbar) to portraits to tablets (Ipad & Samsungcompatiability).

--- components/CategoryHero.tsx
+++ components/CategoryHero.tsx
@@ -35,7 +35,7 @@ export default function CategoryHero({
 
   if (variant === "packs") {
     return (
-      <div className="hidden lg:flex items-end gap-4 shrink-0">
+      <div className={`${styles.heroFlex} items-end gap-4 shrink-0`}>
         {/* Left */}
         <div
           className={styles.hwPackSide}
@@ -116,7 +116,7 @@ export default function CategoryHero({
 
   return (
     <div
-      className="relative hidden shrink-0 lg:block"
+      className={`relative shrink-0 ${styles.heroBlock}`}
       style={{ width: containerWidth, height: containerHeight }}
     >
       {/* Left */}
