f4e6214d2263ec5058233d784572a27b7df29a82
OguzhanBesiktepe <oguzhanbesiktepe@gmail.com>
2026-03-25 19:26:54 -0400

    Add Digital Asset Links and PWA Screenshots.

---
+++ app/src/main/java/app/dueldex/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.dueldex.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
